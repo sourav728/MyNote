2ed953588e390e2774b067f89ca85965964780ce
sourav728 <souravnandi728@outlook.com>
2019-07-08 16:44:17 +0530

    Initial commit

---
+++ app/src/main/java/com/tvd/mynote/Note.kt
@@ -0,0 +1,7 @@
+package com.tvd.mynote
+
+class Note(nodeID:Int, nodeName: String, nodeDesc: String) {
+    var nodeID:Int? = nodeID
+    var nodeName: String? = nodeName
+    var nodeDesc: String? = nodeDesc
+}
